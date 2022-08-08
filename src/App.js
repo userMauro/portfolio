7605a3dad2018c69f8133076d5df8f0220f34011
name <100819836+userMauro@users.noreply.github.com>
2022-08-08 16:23:53 -0300

    add assets

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound.js";
 import Home from "./components/Home/Home.js";
 
+// const lightbulbON = "D:\mamoproblemas\Programming\Portfolio\React Portfolio\react-portfolio\assets\light-bulbON";
+// const lightbulbOFF = "D:\mamoproblemas\Programming\Portfolio\React Portfolio\react-portfolio\assets\light-bulbOFF";
+// const switchON = "D:\mamoproblemas\Programming\Portfolio\React Portfolio\react-portfolio\assets\switchON";
+// const switchOFF = "D:\mamoproblemas\Programming\Portfolio\React Portfolio\react-portfolio\assets\switchOFF.png"
+
+
 function App() {
   return (
     <div className="App">
+
+      {/* LIGHT-BULB */}
+      {/* <div>
+        <img src="D:\mamoproblemas\Programming\Portfolio\React Portfolio\react-portfolio\assets\switchOFF.png" alt='switchOFF'/>
+        <img src={lightbulbOFF} alt='lightbulbOFF'/>
+      </div> */}
 
       {/* NAV */}
       <header>
